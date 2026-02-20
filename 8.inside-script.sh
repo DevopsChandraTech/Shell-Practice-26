473c203ea832a5cd352b756b402327a04689c3dc
DevOpsChandraTech <devopschandra42@gmail.com>
2026-02-20 19:59:43 +0530

    conversation

--- 8.inside-script.sh
+++ 8.inside-script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-START-DATE=$(date +%s)
-END-DATE=$(date +%s)
-TOTAL-TIME=$(($START-DATE-$END-DATE))
+START_TIME=$(date +%s)
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END-TIME-$START-TIME))
 
-echo "the script start time is: $START-DATE"
+echo "the script start time is:$START_TIME"
 
 sleep 10
 
-echo "the script end time is: $END-DATE"
+echo "the script end time is:$END_TIME"
 
-echo "script execution time is $TOTAL-TIME Sec."
+echo "script execution time is $TOTAL_TIME Sec."
