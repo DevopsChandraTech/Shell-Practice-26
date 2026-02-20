b2ae995eb4adcf0a6e9aced85d4bc4cc14a55fd4
DevOpsChandraTech <devopschandra42@gmail.com>
2026-02-20 17:36:21 +0530

    conversation

---
+++ conversations.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "hello Ramesh How are you"
+echo "I am fine How are you Suresh"
+echo "what are you learning Ramesh"
+echo "I am learning Shell Scripting Suresh"
---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "hello $PERSON1 How are you"
+echo "I am fine How are you $PERSON2"
+echo "what are you learning $PERSON1"
+echo "I am learning Shell Scripting $PERSON2"
+
