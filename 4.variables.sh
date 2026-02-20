442f5a4c07b49337fa20e75799631cf1ab315ea8
DevOpsChandraTech <devopschandra42@gmail.com>
2026-02-20 17:43:35 +0530

    conversation

--- 4.variables.sh
+++ 4.variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 PERSON1=$1
 PERSON2=$2
-read "enter first name:"$1
-read "enter second name:"$2
+read -p "enter first name:"$1
+read -p "enter second name:"$2
 
 echo "hello $PERSON1 How are you"
 echo "I am fine How are you $PERSON2"
