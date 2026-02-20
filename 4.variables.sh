fa732f4aee1b2e46776214e8e89ab541f15c12a9
DevOpsChandraTech <devopschandra42@gmail.com>
2026-02-20 17:41:45 +0530

    conversation

---
+++ 4.variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+read "enter first name:"$1
+read "enter second name:"$2
+
+echo "hello $PERSON1 How are you"
+echo "I am fine How are you $PERSON2"
+echo "what are you learning $PERSON1"
+echo "I am learning Shell Scripting $PERSON2"
