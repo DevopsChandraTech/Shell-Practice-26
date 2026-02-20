19a874c8f8be13556a3d073a54f0318807a81cef
DevOpsChandraTech <devopschandra42@gmail.com>
2026-02-20 20:21:35 +0530

    conversation

--- 7.variable-read.sh
+++ 7.variable-read.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "pls enter your PIN NUMBER"
 
-read "PIN_NUMBER" #here pin number is variable
+read "PIN_NUMBER" #here pin number is variable and its shows the pin on display
+read -s "PIN_NUMBER" #here pin number is variable its hides the pin on display
 
 echo "my pin is:$PIN_NUMBER"
