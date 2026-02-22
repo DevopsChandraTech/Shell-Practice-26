07924ef4de723bfd288bb74af1f5cf6c7e0db4a7
DevOpsChandraTech <devopschandra42@gmail.com>
2026-02-22 19:49:59 +0530

    conditions

--- 13.installations.sh
+++ 13.installations.sh
@@ -6,47 +6,26 @@ if [ $USER_ID -ne 0 ]; then
     exit 1 #if other than 0 it is failure
 fi
 
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo "ERROR:: Command $2 Not Found,Please Check and Try Again"
+        exit 1 #if other than 0 it is failure
+    else 
+        echo "SUCCESS:: Proceed with Installation"
+    fi
+}
+
+VALIDATE $? "MYSQL"
 dnf install mysql -y
 
-if [ $? -ne 0 ]; then
-    echo "ERROR:: Command mysql Not Found,Please Check and Try Again"
-    exit 1 #if other than 0 it is failure
-else 
-    echo "SUCCESS:: Proceed with Installation"
-fi
-
-#nginx installation
-
-USER_ID=$(id -u)
-if [ $USER_ID -ne 0 ]; then
-    echo "ERROR:: Proceed User with Root Priviliges"
-    exit 1 #if other than 0 it is failure
-fi
-
+VALIDATE $? "Nginx"
 dnf install nginx -y
 
-if [ $? -ne 0 ]; then
-    echo "ERROR:: Command nginx Not Found,Please Check and Try Again"
-    exit 1 #if other than 0 it is failure
-else 
-    echo "SUCCESS:: Proceed with Installation"
-fi
-
-#mongodb client installation
-
-USER_ID=$(id -u)
-if [ $USER_ID -ne 0 ]; then
-    echo "ERROR:: Proceed User with Root Priviliges"
-    exit 1 #if other than 0 it is failure
-fi
-
+VALIDATE $? "Mongosh"
 dnf install mongodb-mongosh -y
 
-if [ $? -ne 0 ]; then
-    echo "ERROR:: Command mongosh Not Found,Please Check and Try Again"
-    exit 1 #if other than 0 it is failure
-else 
-    echo "SUCCESS:: Proceed with Installation"
-fi
+
+
+
 
 
