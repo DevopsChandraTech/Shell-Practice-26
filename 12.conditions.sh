372791c87fa8cf19d3ca5fd44167e66b78f97d73
DevOpsChandraTech <devopschandra42@gmail.com>
2026-02-22 18:19:45 +0530

    conditions

--- 12.conditions.sh
+++ 12.conditions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NUMBER=$@
 
-if [ $(NUMBER % 2) -eq 0 ]; then
+if [ $((NUMBER % 2)) -eq 0 ]; then
     echo "Given number $NUMBER is even"
 else 
     echo "Given number $NUMBER is odd"
