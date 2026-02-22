a263e2acfea6822226291fe3e8ebde842576dba4
DevOpsChandraTech <devopschandra42@gmail.com>
2026-02-22 21:33:58 +0530

    conditions

--- 15.colour-install.sh
+++ 15.colour-install.sh
@@ -6,41 +6,41 @@ Y="\e[33m"
 N="\e[0m"
 
 if [ $USER_ID -ne 0 ]; then
-    echo -e "$RERROR$N:: RUN COMMAND WITH ROOT USER PRIVILIGES."
+    echo -e "$R ERROR $N:: RUN COMMAND WITH ROOT USER PRIVILIGES."
     exit 1
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e "$RERROR$N:: $2 Command Not Found, Please Check Syntax Once."
+        echo -e "$R ERROR $N:: $2 Command Not Found, Please Check Syntax Once."
         exit 1
     else 
-        echo -e "$GSuccess$N:: Proceed with $2 Installation."
+        echo -e "$G Success $N:: Proceed with $2 Installation."
     fi
 }
 dnf list installed mysql
 if [ $? -ne 0 ]; then
-    echo -e "$R$2$N not installed Proceed with Installation"
+    echo -e "$R $2 $N not installed Proceed with Installation"
     dnf install mysql -y
     VALIDATE $? "MySql"
 else 
-    echo -e "$2 already exist..! $YSkipping$N"
+    echo -e "$2 already exist..! $Y Skipping $N"
 fi
 
 dnf list installed mysql
 if [ $? -ne 0 ]; then
-    echo -e "$R$2$N not installed Proceed with Installation"
+    echo -e "$R $2 $N not installed Proceed with Installation"
     dnf install nginx -y
     VALIDATE $? "Nginx"
 else 
-    echo -e "$2 already exist..! $YSkipping$N"
+    echo -e "$2 already exist..! $Y Skipping $N"
 fi
 
 dnf list installed mysql
 if [ $? -ne 0 ]; then
-    echo -e "$R$2$N not installed Proceed with Installation"
+    echo -e "$R $2 $N not installed Proceed with Installation"
     dnf install python3 -y
     VALIDATE $? "Python"
 else 
-    echo -e "$2 already exist..! $YSkipping$N"
+    echo -e "$2 already exist..! $Y Skipping $N"
 fi
