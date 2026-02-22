161b4ccd28eefe8fc180e22cfabfcb723b970be2
DevOpsChandraTech <devopschandra42@gmail.com>
2026-02-22 17:44:11 +0530

    conditions

--- 11.conditions.sh
+++ 11.conditions.sh
@@ -9,7 +9,7 @@ read NUMBER
 NUMBER=$1
 if [ $NUMBER -gt 10 ]; then
     echo "given number greaterthan 10"
-elif [ $(NUMBER) -eq 10 ]; then
+elif [ $NUMBER -eq 10 ]; then
     echo "given number is equal to 10"
 else 
     echo "given number lessthan 10"
