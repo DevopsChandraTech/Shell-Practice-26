a2c17b99b0df5fa71f6c933c6ae0a4753be298ed
DevOpsChandraTech <devopschandra42@gmail.com>
2026-02-22 19:07:38 +0530

    conditions

--- 13.installations.sh
+++ 13.installations.sh
@@ -3,12 +3,14 @@
 USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ]; then
     echo "ERROR:: Proceed User with Root Priviliges"
+    exit 1 #if other than 0 it is failure
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR:: Command mysql Not Found,Please Check and Try Again"
+    exit 1 #if other than 0 it is failure
 else 
     echo "SUCCESS:: Proceed with Installation"
 fi
@@ -18,12 +20,14 @@ fi
 USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ]; then
     echo "ERROR:: Proceed User with Root Priviliges"
+    exit 1 #if other than 0 it is failure
 fi
 
 dnf install nginx -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR:: Command nginx Not Found,Please Check and Try Again"
+    exit 1 #if other than 0 it is failure
 else 
     echo "SUCCESS:: Proceed with Installation"
 fi
@@ -33,12 +37,14 @@ fi
 USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ]; then
     echo "ERROR:: Proceed User with Root Priviliges"
+    exit 1 #if other than 0 it is failure
 fi
 
 dnf install mongodb-mongosh -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR:: Command mongosh Not Found,Please Check and Try Again"
+    exit 1 #if other than 0 it is failure
 else 
     echo "SUCCESS:: Proceed with Installation"
 fi
