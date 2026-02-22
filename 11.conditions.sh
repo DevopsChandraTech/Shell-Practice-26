2ba303de5d5063b61359627ddc59a66a972d8288
DevOpsChandraTech <devopschandra42@gmail.com>
2026-02-22 17:36:17 +0530

    conditions

---
+++ 11.conditions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# eq = equal
+# -lt = less than
+# -gt = greater than
+# -ne = not equal 
+
+echo "pls enter the number"
+read NUMBER
+NUMBER=$1
+if [ $NUMBER -gt 10 ];then
+    echo "given number greaterthan 10"
+elif [ $NUMBER -eq 10 ];then
+    echo "given number is equal to 10"
+else 
+    echo "given number lessthan 10"
