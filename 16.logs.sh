974b72f9a20a8ae16f449243fc01adecb3ab7036
DevOpsChandraTech <devopschandra42@gmail.com>
2026-02-23 15:10:52 +0530

    shell-scripting

--- 16.logs.sh
+++ 16.logs.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #install mysql nginx python onto server using shell script
 #check user have root privilizes or not
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-USER_ID=$(id -u) # here id =0 root user otherthan 0 normal user
 
+USER_ID=$(id -u) # here id =0 root user otherthan 0 normal user
 if [ $USER_ID -ne 0]; then
     echo -e "$R ERROR $N:: Please run with root user privilizes."
     exit 1
 fi
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 VALIDATE() {
     if [ $1 -ne 0 ]; then
