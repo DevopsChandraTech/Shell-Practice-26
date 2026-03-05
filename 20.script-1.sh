609fb66882026050e224b5453e78841ec671c485
DevOpsChandraTech <devopschandra42@gmail.com>
2026-03-05 19:09:17 +0530

    shell-roboshop

--- 20.script-1.sh
+++ 20.script-1.sh
@@ -2,4 +2,8 @@
 NAME=INDIA
 echo "my country is $NAME"
 echo "Process id of Script is: $$"
-sh 21.script-2.sh
+#sh 21.script-2.sh
+
+source ./21.script-2.sh #here . represents currect directory
+# source checks the path echo $PATH from current directory
+
