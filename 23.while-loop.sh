41dab8924016d814aefd06a095bbe708e3cfd936
DevOpsChandraTech <devopschandra42@gmail.com>
2026-03-08 17:48:18 +0530

    shell-practice

--- 23.while-loop.sh
+++ 23.while-loop.sh
@@ -5,15 +5,21 @@
 #     command to be executed
 # done
 
-count=5
+# count=5
 
-echo "Startig countdown"
+# echo "Startig countdown"
 
-while [ $count -gt 0 ]
-do
-    echo "Time left in $count"
-    sleep 1
-    count=$((count - 1))
-done
+# while [ $count -gt 0 ]
+# do
+#     echo "Time left in $count"
+#     sleep 1
+#     count=$((count - 1))
+# done
 
-echo "TimesUp"
+# echo "TimesUp"
+
+#!/bin/bash
+while IFS= read -r line; do #IFS -> Internal Field Separator
+    # Process each line here
+    echo "$line"
+done < "20.script-1"   
