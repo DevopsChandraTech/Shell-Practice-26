e9d27b242b2b5178cf49969077379e9abde148d6
DevOpsChandraTech <devopschandra42@gmail.com>
2026-03-10 10:54:23 +0530

    shell-practice

---
+++ 24.backup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#install mysql nginx python onto server using shell script
+#check user have root privilizes or not
+
+USER_ID=$(id -u) # here id =0 root user otherthan 0 normal user
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER=/var/log/shell-script
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+mkdir -p $LOGS_FOLDER
+echo "this folder is executed by $(date)"
+
+if [ $USER_ID -ne 0 ]; then
+    echo -e "$R ERROR $N:: Please run with root user privilizes."
+    exit 1
+fi
+
+# the special variable how many arguments passed into script $#
+
+SOURCE_DIR=$(find <LOGS_DIR> -name <name of file> -type f -mtime +14)
+DESTINATION_DIR=$(DEST_DIR)
+
+USAGE(){
+    echo "24.backup.sh <source directory> <destination directory> <no.of days>(optional) default is 14days>"
+}
+
+if [ $# -lt 2 ]; then
+    echo -e "ERROR:: Pass Source and Destination arguments."
+    USAGE
+fi
