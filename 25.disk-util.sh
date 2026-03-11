fa73d80b74a829deb0bc5373206f596badf76672
DevOpsChandraTech <devopschandra42@gmail.com>
2026-03-11 17:57:24 +0530

    shell-practice

--- 25.disk-util.sh
+++ 25.disk-util.sh
@@ -10,9 +10,9 @@ do
    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
    PARTITION=$(echo $line | awk '{print $7}' | cut -d "%" -f1)
    if [ $USAGE -ge $DISK_THRESHOLD ]; then
-        MESSAGE+="High Disk Usage On $PARTITION : $USAGE %" #here + symbol is append lines
+        MESSAGE+="High Disk Usage On $PARTITION : $USAGE % \n" #here + symbol is append lines
    fi
 
 done <<< $DISK_USAGE
 
-echo -e "Message Body = $MESSAGE \n"
+echo -e "Message Body = $MESSAGE"
