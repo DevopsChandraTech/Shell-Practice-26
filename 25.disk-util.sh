3936788f1199fc8b79bc5c657d2d22d2e9e8383d
DevOpsChandraTech <devopschandra42@gmail.com>
2026-03-11 16:55:34 +0530

    shell-practice

--- 25.disk-util.sh
+++ 25.disk-util.sh
@@ -4,7 +4,7 @@ DISK_USAGE=$(df -hT | grep -v Filesystem)
 
 while IFS= read -r line
 do
-    echo line: $line | awk '{ print $6 }'
+    echo line: $line | awk '{ print $7 }'
 
 
 done <<< $DISK_USAGE
