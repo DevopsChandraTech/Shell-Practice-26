15378799c398d2537388977999b515174981b1df
DevOpsChandraTech <devopschandra42@gmail.com>
2026-03-11 17:37:37 +0530

    shell-practice

--- 25.disk-util.sh
+++ 25.disk-util.sh
@@ -5,8 +5,8 @@ THRESHOLD=3
 
 while IFS= read -r line
 do
-   USAGE=$(echo $line | awk '{print $6}')
-   PARTITION=$(echo $line | awk '{print $7}')
+   USAGE=$(echo $line | awk '{print $6}') | cut -d "%" -f1
+   PARTITION=$(echo $line | awk '{print $7}') | cut -d "%" -f1
    if [ $USAGE -ge $THRESHOLD ]; then
     echo "High usage $PARTITION : $USAGE"
    fi
