cd67d80ffa55284eb778592e7c1b0c6f2fc38820
DevOpsChandraTech <devopschandra42@gmail.com>
2026-03-11 17:34:13 +0530

    shell-practice

--- 25.disk-util.sh
+++ 25.disk-util.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
+THRESHOLD=3
 
 while IFS= read -r line
 do
-   echo "$line | awk '{print $6}'"
+   USAGE=$(echo $line | awk '{print $6}')
+   PARTITION=$(echo $line | awk '{print $7}')
+   if [ $USAGE -ge $THRESHOLD]; then
+    echo "High usage $PARTITION : $USAGE"
+   fi
+
 done <<< $DISK_USAGE
